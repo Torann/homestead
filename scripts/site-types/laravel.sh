f0b4e86f3b7f5492862635db8ac923bab537d3f2
Daniel Stainback <torann@gmail.com>
2019-07-22 16:53:51 -0400

    Fix websocket bug

--- scripts/site-types/laravel.sh
+++ scripts/site-types/laravel.sh
@@ -40,7 +40,7 @@ fi
 if [ "${11}" = "true" ]
 then configureWebsockets="
     location /socket {
-        proxy_pass http://websocket;
+        proxy_pass http://127.0.0.1:8022;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection \$connection_upgrade;
